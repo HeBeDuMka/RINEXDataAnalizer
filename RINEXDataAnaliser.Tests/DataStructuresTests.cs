e09585a214d47471a7708f00f348f93b0b73452c
HeBeDuMka <test@mail.ru>
2024-10-11 22:00:39 +0300

    Добавил функцию парсинга данных одного спутника и тест к ней

---
+++ RINEXDataAnaliser.Tests/DataStructuresTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using RINEXDataAnaliser.DataStructures;
+using System.Text.RegularExpressions;
+
+namespace RINEXDataAnaliser.Tests
+{
+	public class DataStructuresTests
+	{
+		[Fact]
+		public void ObsDataParseTest()
+		{
+			Dictionary<string, double> dataDict = new Dictionary<string, double>();
+			dataDict.Add("C1C", 24007193.157);
+            dataDict.Add("L1C", 126158657.09105);
+            dataDict.Add("C1W", 24007192.907);
+            dataDict.Add("C2W", 24007190.898);
+            dataDict.Add("L2W", 98305441.35502);
+            dataDict.Add("C2L", 24007190.822);
+            dataDict.Add("L2L", 98305447.33105);
+            dataDict.Add("C5Q", 24007193.934);
+            dataDict.Add("L5Q", 94209395.09307);
+            dataDict.Add("C1L", 24007192.547);
+            dataDict.Add("L1L", 126158648.07305);
+
+            string data = "G23  24007193.157 5 126158657.09105  24007192.907 2  24007190.898 2  98305441.35502  24007190.822 5  98305447.33105  24007193.934 7  94209395.09307  24007192.547 5 126158648.07305";
+			string regexString = @"G\d{2}\s{1}(?<C1C_quality>\d{1})?\s*(?<C1C>\d{8}.\d{3})\s{1}(?<L1C_quality>\d{1})?\s*(?<L1C>\d{1,9}.\d{5})\s{1}(?<C1W_quality>\d{1})?\s*(?<C1W>\d{8}.\d{3})\s{1}(?<C2W_quality>\d{1})?\s*(?<C2W>\d{8}.\d{3})\s{1}(?<L2W_quality>\d{1})?\s*(?<L2W>\d{1,9}.\d{5})\s{1}(?<C2L_quality>\d{1})?\s*(?<C2L>\d{8}.\d{3})\s{1}(?<L2L_quality>\d{1})?\s*(?<L2L>\d{1,9}.\d{5})\s{1}(?<C5Q_quality>\d{1})?\s*(?<C5Q>\d{8}.\d{3})\s{1}(?<L5Q_quality>\d{1})?\s*(?<L5Q>\d{1,9}.\d{5})\s{1}(?<C1L_quality>\d{1})?\s*(?<C1L>\d{8}.\d{3})\s{1}(?<L1L_quality>\d{1})?\s*(?<L1L>\d{1,9}.\d{5})";
+
+			Regex regex = new Regex(regexString, RegexOptions.Compiled);
+
+			RINEX_obs_satelite_data obs_Satelite_Data = new RINEX_obs_satelite_data(data, regex);
+
+            Assert.Equal(dataDict, obs_Satelite_Data.data);
+        }
+	}
+}
--- RINEXDataAnaliser/DataStructures/RINEX_obs_structs.cs
+++ RINEXDataAnaliser/DataStructures/RINEX_obs_structs.cs
@@ -1,4 +1,7 @@
-﻿namespace RINEXDataAnaliser.DataStructures
+﻿using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace RINEXDataAnaliser.DataStructures
 {
     /// <summary>
     /// Класс для хранения данных о спутниках в пределах одной эпохи
@@ -30,5 +33,35 @@
         /// Словарь хранящий качество псевдофаз
         /// </summary>
         public Dictionary<string, int> phase_quality = new Dictionary<string, int>();
+
+        /// <summary>
+        /// Конструктор класс для парсинга данных одного спутника
+        /// </summary>
+        /// <param name="dataToParse">Данные для парсинга</param>
+        /// <param name="regularException">Регулярное выражения для парсинга</param>
+        public RINEX_obs_satelite_data(string dataToParse, Regex regularException)
+        {
+            // С помощью регулярного выражения разбиваем строку
+            Match match = regularException.Match(dataToParse);
+
+            // Запускаем цикл по полученным результатам
+            foreach (Group group in match.Groups)
+            {
+                // Первым элементом результатов будет изначальная строка, поэтому пропускаем её,
+                // также пропускаем пустые значения
+                if (group.Value != dataToParse && group.Value != "")
+                {
+                    // Если в названии параметра присутствует "quality", то это значение качества
+                    // псевдофазы или псевдодальности
+                    if (group.Name.Contains("quality"))
+                        // Записываем в соответствующий массив
+                        phase_quality.Add(group.Name, Convert.ToInt32(group.Value));
+                    // Если нет, то это данные
+                    else
+                        // Записываем в соответствующий массив
+                        data.Add(group.Name, Convert.ToDouble(group.Value, CultureInfo.InvariantCulture));
+                }
+            }
+        }
     }
 }

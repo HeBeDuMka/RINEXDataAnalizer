84fb159908d070d5687159871906b2e41858dc40
HeBeDuMka <test@mail.ru>
2024-10-12 13:31:43 +0300

    Добавлен класс для общения с FTP сервером и тесты для него

---
+++ RINEXDataAnaliser.Tests/FTPManagerTests.cs
@@ -0,0 +1,44 @@
+﻿using RINEXDataAnaliser.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RINEXDataAnaliser.Tests
+{
+    public class FTPManagerTests
+    {
+        [Fact]
+        public void GetFilesListNoError()
+        {
+            List<string> data = new List<string>();
+            data.Add(".");
+            data.Add("..");
+            data.Add("htdocs");
+            data.Add(".override");
+            data.Add("DO NOT UPLOAD FILES HERE");
+            data.Sort();
+
+            FTPManager ftpManager = new FTPManager("ftp://ftpupload.net", "b7_33706431", "logitech");
+
+            List<string> fileList = ftpManager.getFileList();
+            fileList.Sort();
+
+            Assert.Equal(data, fileList);
+        }
+
+        [Fact]
+        public void GetFilesListError()
+        {
+            List<string> data = new List<string>();
+
+            FTPManager ftpManager = new FTPManager("ftp://ftpupload", "b7_33706431", "logitech");
+
+            List<string> fileList = ftpManager.getFileList();
+
+            Assert.Equal(data, fileList);
+        }
+    }
+}
---
+++ RINEXDataAnaliser/FTPManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RINEXDataAnaliser
+{
+    /// <summary>
+    /// Класс для общения с FTP сервером
+    /// </summary>
+    public class FTPManager
+    {
+        /// <summary>
+        /// Строка для хранения адреса FTP сервера
+        /// </summary>
+        public string ftpUrl = "";
+
+        /// <summary>
+        /// Строка для хранения логина от FTP сервера
+        /// </summary>
+        public string login = "";
+
+        /// <summary>
+        /// Строка для хранения пароля от FTP сервера
+        /// </summary>
+        public string password = "";
+
+        public string curentWorkingDirectory = "/";
+
+        /// <summary>
+        /// Конструктор для задания основных параметров FTP сервера
+        /// </summary>
+        /// <param name="_ftpUrl">Адрес FTP сервера</param>
+        /// <param name="_login">Логин от FTP сервера</param>
+        /// <param name="_password">Пароль от FTP сервера</param>
+        public FTPManager(string _ftpUrl, string _login, string _password)
+        {
+            ftpUrl = _ftpUrl;
+            login = _login;
+            password = _password;
+        }
+
+        /// <summary>
+        /// Метод для подключения к FTP серверу
+        /// </summary>
+        public List<string> getFileList()
+        {
+            List<string> result = new List<string>();
+
+            try
+            {
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpUrl + curentWorkingDirectory);
+                request.Method = WebRequestMethods.Ftp.ListDirectory;
+                request.Credentials = new NetworkCredential(login, password);
+
+                // Задаем параметры соединения
+                request.UsePassive = true;
+                request.UseBinary = true;
+                request.KeepAlive = false;
+
+                // Получаем ответ от сервера
+                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                {
+                    // Открываем поток для чтения данных с FTP сервера
+                    using (Stream responseStream = response.GetResponseStream())
+                    {
+                        // Читаем данные из потока
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+                            string line;
+                            Console.WriteLine("Список файлов и папок на FTP сервере:");
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                result.Add(line);
+                            }
+                        }
+                    }
+
+                    // Выводим статус операции
+                    Console.WriteLine($"Операция завершена, статус: {response.StatusDescription}");
+                }
+
+                return result;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+
+                return new List<string>();
+            }
+        }
+    }
+}

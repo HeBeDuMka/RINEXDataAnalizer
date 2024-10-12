92b96c4c4320cad46b631e2000c845099d6a4897
HeBeDuMka <test@mail.ru>
2024-10-12 11:55:28 +0300

    Добавлен класс для хранения координат XYZ

---
+++ RINEXDataAnaliser/DataStructures/Coordinates.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RINEXDataAnaliser.DataStructures
+{
+    /// <summary>
+    /// Класс для хранения координат в системе XYZ
+    /// </summary>
+    public class XYZCoordinates
+    {
+        /// <summary>
+        /// Координата X
+        /// </summary>
+        public double X;
+        
+        /// <summary>
+        /// Координата Y
+        /// </summary>
+        public double Y;
+        
+        /// <summary>
+        /// Координата Z
+        /// </summary>
+        public double Z;
+
+        public XYZCoordinates()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+        }
+    }
+}

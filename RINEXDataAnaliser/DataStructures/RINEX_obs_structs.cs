f718b9807d400ce05629b0cb060e71c78cb4376f
HeBeDuMka <test@mail.ru>
2024-10-19 19:51:43 +0300

    Добавлен класс для хранения полей заголовка RINEX файла

--- RINEXDataAnaliser/DataStructures/RINEX_obs_structs.cs
+++ RINEXDataAnaliser/DataStructures/RINEX_obs_structs.cs
@@ -64,4 +64,126 @@ namespace RINEXDataAnaliser.DataStructures
             }
         }
     }
+
+    /// <summary>
+    /// Класс для хранения данных заголовка файла наблюдений
+    /// </summary>
+    public class RINEXObsHeaderData
+    {
+        /// <summary>
+        /// Строка с текущей версией RINEX файла
+        /// </summary>
+        public string fileVersion = "";
+
+        /// <summary>
+        /// Спутниковые системы представленные в файле
+        /// </summary>
+        public string sateliteSystems = "";
+
+        /// <summary>
+        /// Имя программы создавшей файл
+        /// </summary>
+        public string programNameCreate = "";
+
+        /// <summary>
+        /// Имя агенства создавшего файл
+        /// </summary>
+        public string agencyNameCreate = "";
+
+        /// <summary>
+        /// Дата и время начала измерений
+        /// </summary>
+        public DateTime dateTimeCreate;
+
+        /// <summary>
+        /// Временная зона в которой был создан файл
+        /// </summary>
+        public string timeZone = "";
+
+        /// <summary>
+        /// Все коментарии из заголовка
+        /// </summary>
+        public List<string> comments = new();
+
+        /// <summary>
+        /// Название маркера антенны
+        /// </summary>
+        public string antennaMarkerName = "";
+
+        /// <summary>
+        /// Номер маркера антенны (опционально)
+        /// </summary>
+        public string antennaMarkerNumber = "";
+
+        /// <summary>
+        /// Тип маркера (геодезический, негеодезический и другие)
+        /// </summary>
+        public string antennaMarkerType = "";
+
+        /// <summary>
+        /// Имя наблюдателя
+        /// </summary>
+        public string observerName = "";
+
+        /// <summary>
+        /// Имя агенства
+        /// </summary>
+        public string agencyName = "";
+
+        /// <summary>
+        /// Имя приемника
+        /// </summary>
+        public string reciverNumber = "";
+
+        /// <summary>
+        /// Тип приемника
+        /// </summary>
+        public string reciverType = "";
+
+        /// <summary>
+        /// Версия приемника
+        /// </summary>
+        public string reciverVersion = "";
+
+        /// <summary>
+        /// Номер антенны
+        /// </summary>
+        public string antennaNumber = "";
+
+        /// <summary>
+        /// Тип антенны
+        /// </summary>
+        public string antennaType = "";
+
+        /// <summary>
+        /// Положение маркера в геоцентрической системе координат, в метрах
+        /// </summary>
+        public XYZCoordinates approxPosition = new XYZCoordinates();
+
+        /// <summary>
+        /// Список наблюдаемых типов
+        /// </summary>
+        public List<RINEXObsTypes> obsTypes = new List<RINEXObsTypes>();
+    }
+
+    /// <summary>
+    /// Класс для хранения наблюдаемых типов
+    /// </summary>
+    public class RINEXObsTypes
+    {
+        /// <summary>
+        /// Код спутниковой группировки
+        /// </summary>
+        public string systemCode = "";
+
+        /// <summary>
+        /// Количество типов наблюдений
+        /// </summary>
+        public int typesCount = 0;
+
+        /// <summary>
+        /// Список наблюдаемых типов
+        /// </summary>
+        public List<string> typesDescriptors = new();
+    }
 }

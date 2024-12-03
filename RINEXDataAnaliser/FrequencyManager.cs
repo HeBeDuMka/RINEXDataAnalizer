e9b2c5492e47b43f4653be11ab4b9320217a046f
HeBeDuMka <12345677s54@gmail.com>
2024-12-03 12:14:30 +0300

    Добавил класс для хранения частот и работы с ними

---
+++ RINEXDataAnaliser/FrequencyManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RINEXDataAnaliser
+{
+    public class FrequencyManager
+    {
+        private static readonly Dictionary<string, double> GPSFrequencyRanges = new Dictionary<string, double>
+        {
+            { "L1", 1575.42 },
+            { "L2", 1227.60 },
+            { "L5", 1176.45 }
+        };
+
+        public static double getGammaGPS()
+        {
+            return Math.Pow(GPSFrequencyRanges["L1"] / GPSFrequencyRanges["L2"], 2);
+        }
+
+        public static (double, double) getK1AndK2CoefsGPS()
+        {
+            double k1, k2;
+
+            k1 = Math.Pow(GPSFrequencyRanges["L1"], 2) / (Math.Pow(GPSFrequencyRanges["L1"], 2) - Math.Pow(GPSFrequencyRanges["L2"], 2));
+            k2 = Math.Pow(GPSFrequencyRanges["L2"], 2) / (Math.Pow(GPSFrequencyRanges["L1"], 2) - Math.Pow(GPSFrequencyRanges["L2"], 2));
+
+            return (k1, k2);
+        }
+    }
+}
